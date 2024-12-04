b69ace43345eec129db5bdfebc2422c7f357d823
yaseen20051 <yi34039@gmail.com>
2024-12-05 00:18:00 +0200

    DataBase
    UserDataBase is added successfully

---
+++ Connect_Hub/DataBase/DataBase.java
@@ -0,0 +1,10 @@
+// using this interface to apply Dependency inversion Principle
+ interface DataBase {
+     void save();
+     void addUser(User user);
+     public void load();
+}
+/*
+user    content
+Dat=Base userDateba  = new
+ */
